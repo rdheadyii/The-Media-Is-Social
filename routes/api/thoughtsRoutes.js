b26726d9765fcc1a071f027c6dbff3b19cdfe59d
rdheadyii <rdheadyii@gmail.com>
2023-08-24 19:20:29 -0400

    wrote routes for thoughts and reactions

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,31 @@
+// import express
+const router = require('express').Router();
+
+// import controllers
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction,
+} = require('../../controllers/thoughtController.js');
+
+//  /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+//  /api/thoughts/:id
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
