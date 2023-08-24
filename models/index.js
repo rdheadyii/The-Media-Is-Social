e5ce9409d5795d8a92d1a1ba41d37d708439c3bd
rdheadyii <rdheadyii@gmail.com>
2023-08-24 08:52:15 -0400

    removed reaction from model index since it isnt a model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // import and declare all models
 const User = require('./User');
 const Thought = require('./Thought');
-const Reaction = require('./Reaction');
+
 
 // export out declared models
-module.exports = {User, Thought, Reaction};
+module.exports = {User, Thought};
